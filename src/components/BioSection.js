2705d94ae3e5b7ce90c8494a71738e6f083421b4
brendanmac001 <atlasmaccc@gmail.com>
2023-05-26 11:31:59 -0400

    fin

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -4,7 +4,6 @@ import './BioSection.css';
 const BioSection = () => {
   return (
     <section className="bio-section">
-      <h2>Brendan McIntosh</h2>
       <p className="bio-description">
       I am a Software Engineer with a strong command of JavaScript and its frameworks like React, as well as proficiency in front-end technologies such as HTML, CSS, and Bootstrap. On the back-end, I am skilled in Node.js, Express, and MongoDB. I also have experience with Django and Python for web development. Additionally, I am familiar with various tools and platforms such as Heroku, Netlify, and Firebase for deployment and hosting.
       </p>
